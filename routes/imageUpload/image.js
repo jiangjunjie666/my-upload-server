5d021cede126aec0247156dfe53f3045278c319c
jiangjunjie666 <2631261619@qq.com>
2023-10-13 22:10:05 +0800

    文件批量上传

--- routes/imageUpload/image.js
+++ routes/imageUpload/image.js
@@ -4,5 +4,7 @@ var router = express.Router()
 const handler = require('./image_handler')
 //挂载路由
 router.post('/imageUpload', handler.imageUp)
+//上传文件
+router.post('/fileUpload', handler.fileUp)
 
 module.exports = router
--- routes/imageUpload/image_handler.js
+++ routes/imageUpload/image_handler.js
@@ -3,7 +3,6 @@
 const formidable = require('formidable')
 const path = require('path')
 const fs = require('fs')
-
 exports.imageUp = (req, res, next) => {
   const form = formidable({
     multiples: true,
@@ -15,12 +14,68 @@ exports.imageUp = (req, res, next) => {
       next(err)
       return
     }
-    console.log(files.file)
-    let url = 'http://127.0.0.1:3000/images/' + files.file.newFilename
+    console.log(files)
+    //切割出上传的文件的后缀名
+    let ext = files.file.mimetype.split('/')[1]
+    //计算出图片文件大小
+    let size = (files.file.size / 1024 / 1024).toFixed(2)
+    if ((ext == 'png' || ext == 'jpg' || ext == 'jpeg' || ext == 'mp4') && size < 2) {
+      let url = 'http://127.0.0.1:3000/images/' + files.file.newFilename
+      res.send({
+        code: 200,
+        msg: '上传成功',
+        imgUrl: url
+      })
+    } else {
+      res.send({
+        code: 400,
+        msg: '只能上传png、jpg、jpeg格式的图片或图片过大'
+      })
+      return
+    }
+  })
+}
+
+exports.fileUp = (req, res, next) => {
+  //上传大小小于10Mb的文件
+  //接收数据
+  const form = formidable({
+    multiples: true,
+    uploadDir: path.join(__dirname, '../../public/file'),
+    keepExtensions: true
+  })
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err)
+      return
+    }
+    //限制上传文件的大小
+    if (files.file.size > 1024 * 1024 * 5) {
+      //删除对应的文件
+      const folderPath = path.join(__dirname, '../../public/file/' + files.file.newFilename) // 文件路径
+      fs.unlinkSync(folderPath)
+      res.send({
+        code: 400,
+        msg: '上传文件过大'
+      })
+      return
+    }
+    //修改保存文件的默认name
+    const folderPath = path.join(__dirname, '../../public/file/' + files.file.newFilename) // 文件路径
+    let newName = path.join(__dirname, '../../public/file/' + files.file.originalFilename)
+
+    //对读取的文件进行重命名
+    fs.rename(folderPath, newName, (err) => {
+      if (err) {
+        console.log(err)
+        return
+      } else {
+        console.log('重命名成功')
+      }
+    })
     res.send({
       code: 200,
-      msg: '上传成功',
-      imgUrl: url
+      msg: '上传成功'
     })
   })
 }
